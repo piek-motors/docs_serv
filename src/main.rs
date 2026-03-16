10bf5215e1ffb47d8c6ebdb06c4c3c47e12ec10f
Fin <loseev5@gmail.com>
2026-03-16 22:35:40 +0300

    fix: use. 0.0.0.0 for running inside container

--- src/main.rs
+++ src/main.rs
@@ -58,7 +58,7 @@ async fn main() {
 }
 
 async fn serve(app: Router, port: u16) {
-    let addr = SocketAddr::from(([127, 0, 0, 1], port));
+    let addr = SocketAddr::from(([0, 0, 0, 0], port));
     let listener: tokio::net::TcpListener = tokio::net::TcpListener::bind(addr).await.unwrap();
     tracing::info!("listening on http://{}", listener.local_addr().unwrap());
     axum::serve(listener, app.layer(TraceLayer::new_for_http()))
